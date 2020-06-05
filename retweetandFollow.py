3e4c3fbc4e456c04fb2d6e0483249a08f873522c
Crystal <crystal.ramirez@ibm.com>
2020-06-05 17:42:29 -0400

    comment out follow option for now and added list of hashtags to choose from

--- retweetandFollow.py
+++ retweetandFollow.py
@@ -1,32 +1,25 @@
 import tweepy
 import time
+import random
 import twitter_credentials
 from tweepy import OAuthHandler
+
 
 
 #auth variables 
 auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
 auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
-
-
-def searchBot():
-    for tweet in tweets:
-        try:
-            tweet.retweet()
-            print("bork bork")
-            time.sleep(30)
-        except tweepy.TweepError as e:
-            print(e.reason)
-            time.sleep(30)
             
-def searchBotCount(hashtag="#doggo", tweetNumber=1):
-    tweets = tweepy.Cursor(api.search, hashtag).items(tweetNumber)
+def searchBotCount(hashtag_list=0, tweetNumber=5):
+    hashtag = ["#Germanshepard", "#Doggo", "#Foster", "#cats", "#dogs"]
+    hashtag_list=random.choice(hashtag)
+    tweets = tweepy.Cursor(api.search, hashtag_list).items(tweetNumber)
     for tweet in tweets:
         try:
             tweet.retweet()
             tweet.favorite()
-            api.create_friendship(tweet.user.id)
+            #api.create_friendship(tweet.user.id)
             print("bork bork")
             time.sleep(1)
         except tweepy.TweepError as e:
@@ -40,4 +33,4 @@ def followMyFollowers():
 
 
 searchBotCount()
-followMyFollowers()
+#followMyFollowers()
